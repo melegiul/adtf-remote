6e41dcf454a9556106d2e8c08d9a20fd8a542032
Reinhard Pröll <reinhard.proell@informatik.uni-augsburg.de>
2020-07-27 23:38:42 +0200

    added trapezoid parameters to car preferences tab

--- src/dialog_preferences.cpp
+++ src/dialog_preferences.cpp
@@ -39,6 +39,14 @@ void PreferencesDialog::showPreferences() {
     this->sb_car_init_orientation->setValue(carSettings.value("odoinit/orientation", 1).toInt());
     this->sb_car_length->setValue(carSettings.value("car/length", 400).toInt());
     this->sb_car_width->setValue(carSettings.value("car/width", 240).toInt());
+    this->sb_left_near_x->setValue(carSettings.value("carview/leftnearx", -300).toInt());
+    this->sb_left_near_y->setValue(carSettings.value("carview/leftneary", 300).toInt());
+    this->sb_right_near_x->setValue(carSettings.value("carview/rightnearx", 300).toInt());
+    this->sb_right_near_y->setValue(carSettings.value("carview/rightneary", 300).toInt());
+    this->sb_left_far_x->setValue(carSettings.value("carview/leftfarx", -300).toInt());
+    this->sb_left_far_y->setValue(carSettings.value("carview/leftfary", 600).toInt());
+    this->sb_right_far_x->setValue(carSettings.value("carview/rightfarx", 300).toInt());
+    this->sb_right_far_y->setValue(carSettings.value("carview/rightfary", 600).toInt());
 }
 
 void PreferencesDialog::savePreferences() {
@@ -54,4 +62,12 @@ void PreferencesDialog::savePreferences() {
     carSettings.setValue("odoinit/orientation", this->sb_car_init_orientation->value());
     carSettings.setValue("car/length", this->sb_car_length->value());
     carSettings.setValue("car/width", this->sb_car_width->value());
+    carSettings.setValue("carview/leftnearx", this->sb_left_near_x->value());
+    carSettings.setValue("carview/leftneary", this->sb_left_near_y->value());
+    carSettings.setValue("carview/rightnearx", this->sb_right_near_x->value());
+    carSettings.setValue("carview/rightneary", this->sb_right_near_y->value());
+    carSettings.setValue("carview/leftfarx", this->sb_left_far_x->value());
+    carSettings.setValue("carview/leftfary", this->sb_left_far_y->value());
+    carSettings.setValue("carview/rightfarx", this->sb_right_far_x->value());
+    carSettings.setValue("carview/rightfary", this->sb_right_far_y->value());
 }
